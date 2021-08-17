cbb465dacb24843f036669b7b789d445fe38658d
djames <davidabraham@ucla.edu>
2021-08-16 18:59:42 -0700

    starting to add core code

--- example/test.jl
+++ example/test.jl
@@ -3,55 +3,71 @@ using Plots
 
 const σ = 5.670374419e-8
 
-L_int(R, Teff, Teq) = 4π*R^2 * σ * (Teff^4 - Teq^4)
-T_eff(T1, B) = (B / T1)^-1.244
+L_f(R, Teff, Teq) = 4π*R^2 * σ * (Teff^4 - Teq^4)
+T_eff(T1, B) = (T1 / B)^(1/1.244)
+T_1(Teff, B) = B * (Teff + 0*im)^1.244
 
 
-function main()
+function fluid()
 
     GyrToSec = 1e9*365*24*60*60
+    GPaToBar = 1e4
+    ∇ = 0.2585
 
     Me = 5.972e24
     Re = 6371e3
-    T1 = 76
+    T = 59.1
+    T1 = 200
+    P0 = 40
 
     M = 6.55*Me
     R = 2.68*Re
-    Cp = 3000
+    Cp = 5000
     Teq = 58.1
 
     B = 0.47529
     T = T_eff(T1, B)
 
-    p = (M, R, Cp, Teq)
-    tspan = (0, 10*GyrToSec)
+    p = (M, R, Cp, Teq, B)
+    tspan = (0., 10.0*GyrToSec)
 
-    function dTdt(T, p, t)
+    function dTdt(Teff, p, t)
 
         M = p[1]
         R = p[2]
         Cp = p[3]
         Teq = p[4]
+        B = p[5]
 
-        Lf = 4π*R^2 * σ * (T^4 - Teq^4)
+        # Teff = T_eff(T, B)
+        # T = T_1(T,B) * (P0 * GPaToBar)^∇
+        # T = (T / B * 1 / (P0 * GPaToBar)^∇)^-1.244
+
+        Lf = 4π*R^2 * σ * (Teff^4 - Teq^4)
 
         dT = -Lf / (Cp * M)
+
 
         return dT
 
     end
 
-
-    prob = ODEProblem(dTdt, T, tspan, p)
+    prob = ODEProblem(dTdt, T1, tspan, p)
     sol = solve(prob, reltol=1e-8, abstol=1e-8)
 
     T = sol[1,:]
     t = sol.t
     t /= GyrToSec
 
-    plot(t, T, xaxis=("Time (Gyr)"), yaxis=("T_eff (K)", (0,140)))
+    display(T)
+
+    plot(t, T, xaxis=("Time (Gyr)"), yaxis=("T_eff (K)", (0,140)),
+         size=(400,400))
+
+end
+
+function fluidcore()
 
 end
 
 
-main()
