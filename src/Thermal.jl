57de608fa05633b099b9dfcccfb2bfbef2f3e462
David James <davidabraham@ucla.edu>
2021-11-22 14:10:34 -0800

    cleaned up PhysicalConstants call, and old functions

--- src/Thermal.jl
+++ src/Thermal.jl
@@ -1,20 +1,10 @@
 module Thermal
 
-const σ = 5.670374419e-8
+using PhysicalConstants.CODATA2018: σ
 
-L_int(R::Real, Teff::Real, Teq::Real) = 4π*R^2 * σ * (Teff^4 - Teq^4)
+export L_int, T_eff
+
+L_int(R::Real, Teff::Real, Teq::Real) = 4π*R^2 * σ.val * (Teff^4 - Teq^4)
 T_eff(T1::Real, B::Real) = (B / T1)^-1.244
-
-function dTdt(dT, T, p, t)
-    M = p[1]
-    R = p[2]
-    Cp = p[3]
-    Teq = p[4]
-
-    Lf = L_int(R, T, Teq)
-
-    dT = -Lf / (Cp * M)
-
-end
 
 end # module
