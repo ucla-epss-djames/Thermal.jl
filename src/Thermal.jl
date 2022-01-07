f269ed5ba7357e8afd03347ba9195ff606df03a7
David James <davidabraham@ucla.edu>
2022-01-07 14:16:57 -0800

    updated license

--- src/Thermal.jl
+++ src/Thermal.jl
@@ -2,7 +2,11 @@ module Thermal
 
 using PhysicalConstants.CODATA2018: σ
 
+export BarToPa, GyrToSec
 export L_int, T_eff
+
+const BarToPa = 1e5
+const GyrToSec = 1e9*365*24*60*60
 
 L_int(R::Real, Teff::Real, Teq::Real) = 4π*R^2 * σ.val * (Teff^4 - Teq^4)
 T_eff(T1::Real, B::Real) = (B / T1)^-1.244
